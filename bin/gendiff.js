647d020ba51abcc6f9d68244ac42d1f11207d7c3
Anton Sekachev <a9185080045@gmail.com>
2023-10-19 05:28:51 +0300

    add new file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,11 @@ const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
+  
 program.parse();
