73b3b100aeb2238499342927e7decb20742eb5e7
Anton Sekachev <a9185080045@gmail.com>
2024-02-29 21:34:08 +0300

    refactor: add const getData

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import getFormat from './formatters/index.js';
 
 const getPath = (way) => path.resolve(process.cwd(), way);
 
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+const getData = (filepath1, filepath2) => {
   const absolutePath1 = getPath(filepath1);
   const absolutePath2 = getPath(filepath2);
 
@@ -16,6 +16,11 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const parsedFile1 = makeParse(content1, filepath1.split('.')[1]);
   const parsedFile2 = makeParse(content2, filepath2.split('.')[1]);
 
+  return { parsedFile1, parsedFile2 };
+};
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const { parsedFile1, parsedFile2 } = getData(filepath1, filepath2);
   const differences = getFormat(compareData(parsedFile1, parsedFile2), formatName);
   return differences;
 };
