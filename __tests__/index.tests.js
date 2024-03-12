f5d980a18dd538c03f7a4a46cbd700154a3af80a
Anton Sekachev <a9185080045@gmail.com>
2024-03-12 22:42:51 +0300

    fix tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,35 +1,20 @@
 import path from 'node:path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const dirname = process.cwd();
+const getFixturePath = (filename) => path.join(dirname, '__fixtures__', filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (fileFormat) => fs.readFileSync(getFixturePath(fileFormat), 'utf-8');
+describe.each([['stylish'], ['plain'], ['json']])('%s formatter', (formatter) => {
+  const filepathOfExpected = getFixturePath(`${formatter}.txt`);
+  const expected = fs.readFileSync(filepathOfExpected, 'utf-8');
 
-const expectedFiles = {
-  stylish: readFile('resultStylish.txt'),
-  plain: readFile('resultPlain.txt'),
-  json: readFile('resultJSON.txt'),
-};
+  test.each([['json'], ['yml']])('%s files', (extension) => {
+    const filepath1 = getFixturePath(`file1.${extension}`);
+    const filepath2 = getFixturePath(`file2.${extension}`);
 
-test.each([
-  ['file1.json', 'file2.json', 'stylish'],
-  ['file1.yml', 'file2.yml', 'plain'],
-  ['file1.json', 'file2.json', 'json'],
-])('Compare %s with %s, format %p', (filepath1, filepath2, format) => {
-  const file1 = getFixturePath(filepath1);
-  const file2 = getFixturePath(filepath2);
+    const result = genDiff(filepath1, filepath2, formatter);
 
-  expect(genDiff(file1, file2, format)).toEqual(expectedFiles[format]);
-});
-
-test('gendiff file1.json, file2.yaml, " "', () => {
-  expect(() => genDiff(getFixturePath('file1.json'), getFixturePath('file2.yaml'), ' ')).toThrow();
-});
-test('gendiff txt file', () => {
-  expect(() => genDiff(getFixturePath('file1.txt'), getFixturePath('file2.txt'))).toThrow();
+    expect(result).toBe(expected);
+  });
 });
