f8f24b25e70989cd02b9ffb80646852c82ec989a
Anton Sekachev <a9185080045@gmail.com>
2024-04-15 23:38:59 +0300

    refactoring

--- src/getDifferenceTree.js
+++ src/getDifferenceTree.js
@@ -5,43 +5,41 @@ const getDifferenceTree = (obj1, obj2) => {
   const keysMass = _.union(getKeys(obj1), getKeys(obj2));
   const last = _.sortBy(keysMass);
   return last.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
     const obj1HasKey = _.has(obj1, key);
     const obj2HasKey = _.has(obj2, key);
-    const obj1KeyIsObject = _.isPlainObject(value1);
-    const obj2KeyIsObject = _.isPlainObject(value2);
+    const obj1KeyIsObject = _.isPlainObject(obj1[key]);
+    const obj2KeyIsObject = _.isPlainObject(obj2[key]);
     if (obj1HasKey && obj2HasKey && obj1KeyIsObject && obj2KeyIsObject) {
       return {
         type: 'parent',
         key,
-        children: getDifferenceTree(value1, value2),
+        children: getDifferenceTree(obj1[key], obj2[key]),
       };
     } if (obj1HasKey && obj2HasKey && obj1KeyIsObject && !obj2KeyIsObject) {
       return {
         type: 'diffValue',
         key,
-        children: value1,
-        children2: value2,
+        children: obj1[key],
+        children2: obj2[key],
       };
     } if (obj1HasKey && obj2HasKey && !obj1KeyIsObject && obj2KeyIsObject) {
       return {
         type: 'diffValue',
         key,
-        children: value1,
-        children2: value2,
+        children: obj1[key],
+        children2: obj2[key],
       };
     } if (obj1HasKey && !obj2HasKey && obj1KeyIsObject) {
       return {
         type: 'deleted',
         key,
-        children: value1,
+        children: obj1[key],
       };
     } if (!obj1HasKey && obj2HasKey && obj2KeyIsObject) {
       return {
         type: 'added',
         key,
-        children: value2,
+        children: obj2[key],
       };
     } if (obj1HasKey && obj2HasKey) {
       if (obj1[key] === obj2[key]) {
