3e994ae5ebf78b06774ee8a99a44acd39f24637b
Anton Sekachev <a9185080045@gmail.com>
2024-05-13 19:55:18 +0300

    refactoring

--- src/getDifferenceTree.js
+++ src/getDifferenceTree.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import _ from 'lodash';
 
 const getDifferenceTree = (obj1, obj2) => {
@@ -6,39 +5,65 @@ const getDifferenceTree = (obj1, obj2) => {
   const keysMass = _.union(getKeys(obj1), getKeys(obj2));
   const last = _.sortBy(keysMass);
   return last.map((key) => {
-    if (_.has(obj1, key) && _.has(obj2, key) && _.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+    if (
+      _.has(obj1, key)
+      && _.has(obj2, key)
+      && _.isPlainObject(obj1[key])
+      && _.isPlainObject(obj2[key])
+    ) {
       return {
         type: 'parent',
         key,
         children: getDifferenceTree(obj1[key], obj2[key]),
       };
-    } if (_.has(obj1, key) && _.has(obj2, key) && _.isPlainObject(obj1[key]) && !_.isPlainObject(obj2[key])) {
+    }
+
+    if (
+      (_.has(obj1, key)
+        && _.has(obj2, key)
+        && _.isPlainObject(obj1[key])
+        && !_.isPlainObject(obj2[key]))
+      || (_.has(obj1, key)
+        && _.has(obj2, key)
+        && !_.isPlainObject(obj1[key])
+        && _.isPlainObject(obj2[key]))
+    ) {
       return {
         type: 'diffValue',
         key,
         oldValue: obj1[key],
         newValue: obj2[key],
       };
-    } if (_.has(obj1, key) && _.has(obj2, key) && !_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return {
-        type: 'diffValue',
-        key,
-        oldValue: obj1[key],
-        newValue: obj2[key],
-      };
-    } if (_.has(obj1, key) && !_.has(obj2, key) && _.isPlainObject(obj1[key])) {
+    }
+
+    if (_.has(obj1, key)
+     && !_.has(obj2, key)
+     && _.isPlainObject(obj1[key])) {
       return {
         type: 'deleted',
         key,
         oldValue: obj1[key],
       };
-    } if (!_.has(obj1, key) && _.has(obj2, key) && _.isPlainObject(obj2[key])) {
+    }
+    if (!_.has(obj1, key)
+     && _.has(obj2, key)
+     && _.isPlainObject(obj2[key])) {
       return {
         type: 'added',
         key,
         oldValue: obj2[key],
       };
-    } if (_.has(obj1, key) && _.has(obj2, key)) {
+    }
+
+    if (_.has(obj1, key)) {
+      if (!_.has(obj2, key)) {
+        return {
+          type: 'deleted',
+          key,
+          oldValue: obj1[key],
+        };
+      }
+
       if (obj1[key] === obj2[key]) {
         return {
           type: 'stay same',
@@ -46,19 +71,15 @@ const getDifferenceTree = (obj1, obj2) => {
           oldValue: obj1[key],
         };
       }
+
       return {
         type: 'diffValue',
         key,
         oldValue: obj1[key],
         newValue: obj2[key],
       };
-    } if (_.has(obj1, key) && !_.has(obj2, key)) {
-      return {
-        type: 'deleted',
-        key,
-        oldValue: obj1[key],
-      };
     }
+
     return {
       type: 'added',
       key,
